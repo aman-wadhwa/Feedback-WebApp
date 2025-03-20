f6e2402adad75028c60fe084f0f0e3fc0e9eb007
Aman Wadhwa <amanwadhwa0707@gmail.com>
2025-03-20 13:02:42 +0530

    remvoing json dependencies temporarily for deployment

--- src/context/feedbackContext.js
+++ src/context/feedbackContext.js
@@ -1,5 +1,5 @@
-import { useState, createContext, useEffect } from "react";
-// import { v4 as uuidv4 } from "uuid"
+import { useState, createContext } from "react";
+import { v4 as uuidv4 } from "uuid"
 
 
 
@@ -7,30 +7,47 @@ const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({children}) => {
 
-    const [feedback, setFeedback] = useState([])
-    const[isLoading, setisLoading] = useState(true)
+    const [feedback, setFeedback] = useState([
+        {
+            id:1,
+            text:"The food arrived hot and on time! The packaging was great, but the portion size was a bit smaller than expected. Overall, a good experience!",
+            rating:8
+        },
+        {
+            id:2,
+            text:"Excellent service! The mechanic was knowledgeable, explained everything clearly, and the repairs were done quickly. My car runs like new again!",
+            rating:10
+        },
+        {
+            id:3,
+            text:"Frequent connection drops and slow speeds despite paying for a premium plan. Customer support was polite but couldn’t resolve my issue.",
+            rating:4
+        }
+    ])
 
-    useEffect(()=>{
-        fetchFeedback() 
-    }, [])
+    // const[isLoading, setisLoading] = useState(true)
+
+    // useEffect(()=>{
+    //     fetchFeedback() 
+    // }, [])
 
     //fetch api fetchin feedback from json 
-    const fetchFeedback = async () =>{
-        // const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
-        const response = await fetch("/feedback?_sort=id&_order=desc") // we can do this because we have added a proxy in package.json
-        const data = await response.json();
-        setFeedback(data);
-        setisLoading(false)
-    }
+    // const fetchFeedback =  () =>{
+    //     // const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+    //     const response = await fetch("/feedback?_sort=id&_order=desc") // we can do this because we have added a proxy in package.json
+    //     const data = await response.json();
+    //     setFeedback(data);
+    //     setisLoading(false)
+    // }
 
     const [FeedbackEdit, setFeedBackEdit] = useState({
         item:{},
         edit:false
     })
-
-    const deleteFeedback = async (id) => {
+    // doasync
+    const deleteFeedback =  (id) => {
         if(window.confirm('Do you want to delete this feedback?')){
-            await fetch(`/feedback/${id}`, {method:'DELETE'})
+            // await fetch(`/feedback/${id}`, {method:'DELETE'})
             setFeedback(feedback.filter((item)=> item.id!==id));
         }  
     }
@@ -41,41 +58,41 @@ export const FeedbackProvider = ({children}) => {
             edit:true
         })
     }
+    // do async
+    const updateFeedback = (id, newitem) => {
+        // const response = await fetch(`feedback/${id}`,{
+        //     method:'PUT',
+        //     headers:{
+        //         'Content-Type' : 'application/json'
+        //     },
+        //     body : JSON.stringify(newitem)
+        // })
 
-    const updateFeedback = async (id, newitem) => {
-        const response = await fetch(`feedback/${id}`,{
-            method:'PUT',
-            headers:{
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify(newitem)
-        })
-
-        let data = await response.json();
+        // let data = await response.json();
         setFeedback(feedback.map((item)=>{
-           return  id===item.id ? {...item, ...data } : item
+           return  id===item.id ? {...item, ...newitem } : item
         }))
         FeedbackEdit.edit=false;
     }
-    
-    const addFeedback = async (newfeedback) =>{
-        const response = await fetch('/feedback',{
-            method : 'POST',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify(newfeedback)
-        })
-        const data = await response.json()
-        // newfeedback.id = uuidv4()
-        // setFeedback([newfeedback,...feedback])
-        setFeedback([data,...feedback])
+    //do async
+    const addFeedback = (newfeedback) =>{
+        // const response = await fetch('/feedback',{
+        //     method : 'POST',
+        //     headers : {
+        //         'Content-Type' : 'application/json'
+        //     },
+        //     body: JSON.stringify(newfeedback)
+        // })
+        // const data = await response.json()
+        newfeedback.id = uuidv4()
+        setFeedback([newfeedback,...feedback])
+        // setFeedback([data,...feedback])
     }
     
     return <FeedbackContext.Provider value={{
         feedback,
         FeedbackEdit,
-        isLoading,
+        // isLoading,
         addFeedback,
         deleteFeedback, 
         editFeedback,
